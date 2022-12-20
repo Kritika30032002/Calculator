6584825242029ac4a7ad5bd0b1bc8293e4ab557a
Aashish Kumar <92418011+Aashish-Kmr@users.noreply.github.com>
2022-12-20 22:53:33 +0530

    Update index.js
    Added functionalities for power, log, ln and squareroot

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
 let screenValue = '';
+function evaluate(str){
+    if(str.includes("ln")){
+            var parts = str.split("ln",2);
+            str=parts[0]+Math.log(eval(parts[1]));
+}
+    else if(str.includes("log")){
+            var parts = str.split("log",2);
+            str=parts[0]+(Math.log(eval(parts[1]))/Math.log(10));
+    }
+    else if(str.includes("√")){
+        var parts = str.split("√",2);
+        str=parts[0]+Math.sqrt(eval(parts[1]));
+    }
+    str=eval(str);
+    return str;
+}
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
@@ -10,12 +26,32 @@ for (item of buttons) {
             screenValue += buttonText;
             screen.value = screenValue;
         }
+        else if(buttonText == '^') {
+            buttonText = '**';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText == 'log') {
+            buttonText = 'log';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText == 'ln') {
+            buttonText = 'ln';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText == '√') {
+            buttonText = '√';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
         else if (buttonText == 'C') {
             screenValue = "";
             screen.value = screenValue;
         }
         else if (buttonText == '=') {
-            screen.value = eval(screenValue);
+            screen.value = evaluate(screenValue);
         }
         else {
             screenValue += buttonText;
