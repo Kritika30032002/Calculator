5ead5e8fb67377fec6582a527f06b3fc00bd0562
29deepanshutyagi <123951725+29deepanshutyagi@users.noreply.github.com>
2023-05-02 18:56:41 +0530

    new funtions added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
 let screenValue = '';
 let actualValue = '';
+let isDegree = true;
+
 function evaluate(str){
     if(str.includes("ln")){
             var parts = str.split("ln",2);
@@ -32,12 +34,42 @@ for (item of buttons) {
             actualValue += "**";
             screen.value = screenValue;
         }
-        else if (buttonText == 'C') {
+        else if(buttonText == 'e^x') {
+            screenValue+=buttonText;
+            actualValue +=Math.exp(parseFloat(screenValue));
+            screen.value = screenValue;
+        }
+        else  if (buttonText == 'N !') {
+            actualValue = parseInt(screenValue);
+            let factorial = 1;
+            for (let i = 1; i <= actualValue; i++) {
+              factorial *= i;
+            }
+            screenValue = factorial;
+            screen.value = screenValue;
+          }
+        else if (buttonText == 'AC') {
             screenValue = "";
             actualValue = "";
             screen.value = screenValue;
         }
-        else if (buttonText == '=') {
+        else  if (buttonText == 'Cancel') {
+            screenValue = screenValue.slice(0, -1); // Remove last character
+            screen.value = screenValue;
+          }
+        else if (buttonText == 'sin') {
+            let result = Math.sin(isDegree ? parseFloat(screenValue) * Math.PI / 180 : parseFloat(screenValue));
+            screen.value = result.toFixed(2);
+          }
+          else if (buttonText == 'cos') {
+            let result = Math.cos(isDegree ? parseFloat(screenValue) * Math.PI / 180 : parseFloat(screenValue));
+            screen.value = result.toFixed(2);
+          } else if (buttonText == 'tan') {
+            let result = Math.tan(isDegree ? parseFloat(screenValue) * Math.PI / 180 : parseFloat(screenValue));
+            screen.value = result.toFixed(2);
+          }
+       
+        else if (buttonText == '=') { 
             screen.value = evaluate(actualValue);
         }
         else {
